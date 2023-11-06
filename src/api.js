96017db07e0d628477a71ca627f3f3f09f53bc62
Harim Kim <rimiha9708@gmail.com>
2023-11-06 22:01:32 +0900

    weather app 2

--- src/api.js
+++ src/api.js
@@ -12,5 +12,7 @@ const instance = axios.create({
 export const getWeather = () => {
   const lat = 35.15798233915634;
   const lon = 129.0599620036169;
-  return instance.get(`weather?lat=${lat}&lon=${lon}`);
+  return instance
+    .get(`weather?lat=${lat}&lon=${lon}`)
+    .then((response) => response.data);
 };
